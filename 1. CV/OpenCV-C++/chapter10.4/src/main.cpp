ecc4a5d35fb824743fec959c6e53cb04e8afa330
JasonSloan <kungedefaxing@gmail.com>
2023-10-27 10:42:39 +0800

    Change the dir of C++ NanoLog

---
+++ 1. CV/OpenCV-C++/chapter10.4/src/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+#include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+
+int main() {
+
+    return 0;
+}
