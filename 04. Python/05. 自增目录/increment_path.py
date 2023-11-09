90daf56aa88f2832c1e4a2d4794bfbfe3638bbec
JasonSloan <kungedefaxing@gmail.com>
2023-11-09 13:10:12 +0800

    Add Python 自增目录

---
+++ 04. Python/05. 自增目录/increment_path.py
@@ -0,0 +1,23 @@
+import os
+from pathlib import Path
+
+
+def increment_path(path, exist_ok=False, sep='', mkdir=False):
+    # Increment file or directory path, i.e. runs/exp --> runs/exp{sep}2, runs/exp{sep}3, ... etc.
+    path = Path(path)  # os-agnostic
+    if path.exists() and not exist_ok:
+        path, suffix = (path.with_suffix(''), path.suffix) if path.is_file() else (path, '')
+
+        for n in range(2, 9999):
+            p = f'{path}{sep}{n}{suffix}'  # increment path
+            if not os.path.exists(p):  #
+                break
+        path = Path(p)
+
+    if mkdir:
+        path.mkdir(parents=True, exist_ok=True)  # make directory
+
+
+if __name__ == '__main__':
+    path = "runs"
+    increment_path(path)
