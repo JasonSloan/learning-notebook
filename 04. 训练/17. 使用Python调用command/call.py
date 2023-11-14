7c4aaa6eeda16386cbcc4277688ee6f3e0983780
JasonSloan <kungedefaxing@gmail.com>
2023-11-14 10:02:38 +0800

    Add 训练 使用Python调用command

---
+++ 04. 训练/17. 使用Python调用command/call.py
@@ -0,0 +1,39 @@
+import subprocess
+
+
+def colorstr(*input):
+    """彩色打印"""
+    # Colors a string https://en.wikipedia.org/wiki/ANSI_escape_code, i.e.  colorstr('blue', 'hello world')
+    *args, string = input if len(input) > 1 else ('blue', 'bold', input[0])  # color arguments, string
+    colors = {
+        'black': '\033[30m',  # basic colors
+        'red': '\033[31m',
+        'green': '\033[32m',
+        'yellow': '\033[33m',
+        'blue': '\033[34m',
+        'magenta': '\033[35m',
+        'cyan': '\033[36m',
+        'white': '\033[37m',
+        'bright_black': '\033[90m',  # bright colors
+        'bright_red': '\033[91m',
+        'bright_green': '\033[92m',
+        'bright_yellow': '\033[93m',
+        'bright_blue': '\033[94m',
+        'bright_magenta': '\033[95m',
+        'bright_cyan': '\033[96m',
+        'bright_white': '\033[97m',
+        'end': '\033[0m',  # misc
+        'bold': '\033[1m',
+        'underline': '\033[4m'}
+    return ''.join(colors[x] for x in args) + f'{string}' + colors['end']
+    
+    
+ if __name__ == "__main__":
+    command = ['find / -name cuda | grep cuda']
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+    print(colorstr('Standard Out:'))
+    print(result.stdout)
+    print(colorstr('Standard Error:'))
+    print(result.stderr)
+    print(colorstr('Standard Code:'))
+    print(result.returncode)
