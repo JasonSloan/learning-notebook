e6a70b498dd57ae1389da09a41f118d45d94548c
JasonSloan <kungedefaxing@gmail.com>
2023-11-27 15:12:17 +0800

    ReID

---
+++ 04. 训练/19. 各种距离/03. 余弦距离/cosine_dist.py
@@ -0,0 +1,14 @@
+
+
+def compute_cosine_distance(features, others):
+    """Computes cosine distance.
+    Args:
+        features (torch.Tensor): 2-D feature matrix.
+        others (torch.Tensor): 2-D feature matrix.
+    Returns:
+        torch.Tensor: distance matrix.
+    """
+    features = F.normalize(features, p=2, dim=1)
+    others = F.normalize(others, p=2, dim=1)
+    dist_m = 1 - torch.mm(features, others.t())
+    return dist_m.cpu().numpy()
