d1cc80798b60cc6c76eb0a8ebb2d4a845f158612
JasonSloan <kungedefaxing@gmail.com>
2024-03-27 09:37:48 +0800

    Add C++ Timer

---
+++ 07. C++/16. 计时代码(easy-timer)/main.cpp
@@ -0,0 +1,57 @@
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <chrono>
+#include <thread>
+
+
+class TIMER{
+    private:
+        struct timeval start_time, stop_time;
+        double __get_us(struct timeval t) { return (t.tv_sec * 1000000 + t.tv_usec); }
+        char indent[40];
+
+    public:
+        TIMER(){}
+        ~TIMER(){}
+
+        void indent_set(char* s){
+            strcpy(indent, s);
+        }
+        void indent_set(const char* s){
+            strcpy(indent, s);
+        }
+
+        void tik(){
+            gettimeofday(&start_time, NULL);
+        }
+
+        void tok(){
+            gettimeofday(&stop_time, NULL);
+        }
+
+        void print_time(char* str){
+            printf("%s", indent);
+            printf("%s use: %f ms\n", str, get_time());
+        }
+        void print_time(const char* str){
+            printf("%s", indent);
+            printf("%s use: %f ms\n", str, get_time());
+        }
+
+        float get_time(){
+            return (__get_us(stop_time) - __get_us(start_time))/1000;
+        }
+};
+
+
+int main () {
+    TIMER timer;
+    timer.indent_set("Image: ");
+    timer.tik();
+    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    timer.tok();
+    timer.print_time("infer time:");
+    return 0;
+}
