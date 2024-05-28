d9d101a0bbb07809919987f16f239490fcc6fa28
JasonSloan <kungedefaxing@gmail.com>
2024-05-28 15:32:34 +0800

    update C++

--- 08. C++/23. 纯头文件使用方式的第三方库pybind11/example.cpp
+++ 08. C++/23. 纯头文件使用方式的第三方库pybind11/example.cpp
@@ -1,10 +1,15 @@
+#include <cstring>
 #include "opencv2/opencv.hpp"
 #include "pybind11.hpp"
 
 using namespace std;
+using namespace cv;
 namespace py = pybind11;
 
 int main(){
-    py::array& imageArray;
-    cv::Mat cvimage(imageArray.shape(0), imageArray.shape(1), CV_8UC3, (unsigned char*)imageArray.data(0));
+	// todo: 未运行成功
+    Mat img = imread("dog.jpg");
+    auto numel = img.rows * img.cols * img.channels();
+    py::array imageArray;
+    memcpy((uchar*)(imageArray.data(0)), img.data, numel);
 };
