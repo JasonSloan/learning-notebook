b07c7e2c1ab4805d17267d41c7a8f7a6984c385c
JasonSloan <kungedefaxing@gmail.com>
2024-05-29 14:56:41 +0800

    update sth

--- 08. C++/23. 纯头文件使用方式的第三方库pybind11/example.cpp
+++ 08. C++/23. 纯头文件使用方式的第三方库pybind11/example.cpp
@@ -7,7 +7,9 @@ using namespace cv;
 namespace py = pybind11;
 
 int main(){
-	// todo: 未运行成功
+	// todo: 未运行成功, 
+	// 参考本仓库中的"03. 推理与优化加速\01. TensorRT\01. trtpy docs\任务50_使用pybind11使python可以调用C++代码"
+	// 也许pybind11无法在main函数中运行?
     Mat img = imread("dog.jpg");
     auto numel = img.rows * img.cols * img.channels();
     py::array imageArray;
