1e3ede7a918b4718f5c403f4e32f61e239e2f640
JasonSloan <kungedefaxing@gmail.com>
2024-05-30 13:22:39 +0800

    update bytetrack

--- 01. CV/04. 目标跟踪/ByteTrack/可独立运行的YOLOv5+ByteTrack/main.py
+++ 01. CV/04. 目标跟踪/ByteTrack/可独立运行的YOLOv5+ByteTrack/main.py
@@ -24,7 +24,7 @@ def track(frame, tgt_cls, detector, tracker):
     return tracks
 
 
-def synthesis_video_with_trackids(video_path, frame_ids, tlwhs, trackids, output_video_path):
+def synthesis_video_with_trackids(video_path, frame_ids, tlwhs, trackids, output_video_path, draw_trajectory):
     cap = cv2.VideoCapture(video_path)
     fps = cap.get(cv2.CAP_PROP_FPS)
     frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -33,6 +33,7 @@ def synthesis_video_with_trackids(video_path, frame_ids, tlwhs, trackids, output
     out = cv2.VideoWriter(output_video_path, fourcc, fps, (frame_width, frame_height))
     frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
     pbar = tqdm(range(int(frame_count)), desc="Synthesis video")
+    trajectory_points = {}
     for i in pbar:
         ret, frame = cap.read()
         if not ret:
@@ -40,19 +41,27 @@ def synthesis_video_with_trackids(video_path, frame_ids, tlwhs, trackids, output
         if i not in frame_ids:
             continue
         index = frame_ids.index(i)
-        tlwh = tlwhs[index]
-        tid = trackids[index]
-        for j in range(len(tlwh)):
-            point1 = (int(tlwh[j][0]), int(tlwh[j][1]))
-            point2 = (int(tlwh[j][0] + tlwh[j][2]), int(tlwh[j][1] + tlwh[j][3]))
+        itlwhs = tlwhs[index]
+        itids = trackids[index]
+        for j in range(len(itlwhs)):
+            point1 = (int(itlwhs[j][0]), int(itlwhs[j][1]))
+            point2 = (int(itlwhs[j][0] + itlwhs[j][2]), int(itlwhs[j][1] + itlwhs[j][3]))
             cv2.rectangle(frame, point1, point2, (0, 0, 255), 2)
-            cv2.putText(frame, str(tid[j]), (int(tlwh[j][0]), int(tlwh[j][1]) - 10), 0, 1, (0, 255, 0), 2)
+            cv2.putText(frame, str(itids[j]), (int(itlwhs[j][0]), int(itlwhs[j][1]) - 10), 0, 1, (0, 255, 0), 2)
+            if itids[j] not in trajectory_points.keys():
+                trajectory_points[itids[j]] = []
+            xc, yc = (int(itlwhs[j][0] + itlwhs[j][2] / 2), int(itlwhs[j][1] + itlwhs[j][3] / 2))
+            trajectory_points[itids[j]].append([xc, yc])
+            if len(trajectory_points[itids[j]]) > 1:
+                for i in range(1, len(trajectory_points[itids[j]])):
+                    cv2.line(frame, trajectory_points[itids[j]][i-1], trajectory_points[itids[j]][i], (0, 255, 0), 2)
         out.write(frame)
     out.release()
     cap.release()
 
 
-def main(video_path, cfg, weight, tgt_cls, conf_thre, iou_thre, input_size, track_thresh, match_thresh, track_buffer, min_box_area, output_video_path):
+def main(video_path, cfg, weight, tgt_cls, conf_thre, iou_thre, input_size, track_thresh, 
+         match_thresh, track_buffer, min_box_area, output_video_path, draw_trajectory):
     detector = InferenceController(cfg, weight, conf_thre, iou_thre, input_size)
     tracker = BYTETracker(track_thresh, match_thresh, track_buffer, frame_rate=30)
 
@@ -84,26 +93,27 @@ def main(video_path, cfg, weight, tgt_cls, conf_thre, iou_thre, input_size, trac
         video_bar.set_description(f"Frame {vi}: {curr_track_counts} tracks")
     cap.release()
 
-    synthesis_video_with_trackids(video_path, frame_ids, online_tlwhs, online_ids, output_video_path)
+    synthesis_video_with_trackids(video_path, frame_ids, online_tlwhs, online_ids, output_video_path, draw_trajectory)
         
 
 def parse_args():
     parser = ArgumentParser(description="Tracking demo. 注意!!!!!!权重文件应该只保存state_dict而不是整个模型, 否则会报错")
-    parser.add_argument("--video_path", type=str, default="videos/palace.mp4",help="Path to your video")
-    parser.add_argument("--cfg", type=str, default="yolov5-bytetrack/cfg/yolov5s.yaml", help="Path to the yolov5 detector config file")
-    parser.add_argument("--weight", type=str, default="yolov5-bytetrack/weights/yolov5s.pt", help="Path to the yolov5 detector weight()")
+    parser.add_argument("--video_path", type=str, default="demo.mp4",help="Path to your video")
+    parser.add_argument("--cfg", type=str, default="yolov5s.yaml", help="Path to the yolov5 detector config file")
+    parser.add_argument("--weight", type=str, default="yolov5s.pt", help="Path to the yolov5 detector weight()")
     parser.add_argument("--tgt_cls", type=int, default=0, help="Target class index to track(default 0 means track person only)")
     parser.add_argument("--conf_thre", type=float, default=0.01, help="Confidence threshold of the detector(this value should be extremely small)")
-    parser.add_argument("--iou_thre", type=float, default=0.4, help="Detectior nms iou threshold")
+    parser.add_argument("--iou_thre", type=float, default=0.5, help="Detectior nms iou threshold")
     parser.add_argument("--input_size", type=int, nargs='+', default=[384, 640], help="Detector input size")
-    parser.add_argument("--track_thresh", type=float, default=0.3, help="Threshold for the score of the track, greater than it will be considered as high scores detections")
+    parser.add_argument("--track_thresh", type=float, default=0.4, help="Threshold for the score of the track, greater than it will be considered as high scores detections")
     parser.add_argument("--match_thresh", type=float, default=0.9, help="Threshold for the iou between boxes of two frames, " 
                         "if the iou between box0 from T0 and box1 from T1 is greater than it, then we say they are matched")
     parser.add_argument("--track_buffer", type=int, default=30, help="Number of frames that a track is kept without detection,"
                         "lost tracks exceed ? frames will be deleted")
     parser.add_argument("--min_box_area", type=int, default=100, help="The min area of a bounding box to be considered as a target")
-    parser.add_argument("--output_video_path", type=str, default="synthesis_video.mp4", help="Path to the output video")
-
+    parser.add_argument("--output_video_path", type=str, default="outputs/synthesis_video.mp4", help="Path to the output video")
+    parser.add_argument("--draw_trajectory", action="store_true", help="draw trajectory on the synthesis_video or not")
+    
     return parser.parse_args()
 
 
