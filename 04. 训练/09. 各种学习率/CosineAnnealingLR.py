29a29e523eb33ee13706ecb1cfeaaac62fdd9206
JasonSloan <kungedefaxing@gmail.com>
2024-06-04 14:03:03 +0800

    update lr

---
+++ 04. 训练/09. 各种学习率/CosineAnnealingLR.py
@@ -0,0 +1,26 @@
+import torch.nn as nn
+from torch.optim import Adam
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from torchvision.models import resnet18
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    epochs = 100
+    init_lr = 0.1
+    min_lr = 0.001
+    resnet = resnet18()
+    optimizer = Adam(resnet.parameters(), lr=init_lr)
+    lr_scheduler = CosineAnnealingLR(optimizer, epochs, eta_min=min_lr)
+    lrs = []
+    for i in range(epochs):
+        current_lr = lr_scheduler.get_lr()
+        lr_scheduler.step()
+        lrs.append(current_lr)
+    plt.figure()
+    plt.plot(list(range(epochs)), lrs, marker='o')
+    plt.title('CosineAnnealingLR')
+    plt.xlabel('epochs')
+    plt.ylabel('lr')
+    plt.legend()
+    plt.savefig("lr.jpg")
---
+++ 04. 训练/09. 各种学习率/README.md
@@ -0,0 +1,11 @@
+### 1. 线性衰减学习率
+
+![](assets/linear.jpg)
+
+### 2. one-cycle学习率
+
+![](assets/one_cycle.jpg)
+
+### 3. 余弦退火学习率
+
+![](assets/cosine.jpg)
